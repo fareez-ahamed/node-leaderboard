af5f59574ee7870a217399d856e8a70aa6e63af9
Fareez Ahamed <fareez.ahamed@gmail.com>
2022-08-22 18:41:53 +0530

    Added points rewarding logic

--- ui/src/api.ts
+++ ui/src/api.ts
@@ -1,18 +1,26 @@
-export const getLeaderboard = () => {
-  return Promise.resolve([
-    { id: 32, name: "Fareez Ahamed", points: 348 },
-    { id: 32, name: "Fareez Ahamed", points: 348 },
-    { id: 32, name: "Fareez Ahamed", points: 348 },
-    { id: 32, name: "Fareez Ahamed", points: 348 },
-    { id: 34, name: "Fareez Ahamed", points: 348 },
-    { id: 34, name: "Fareez Ahamed", points: 348 },
-    { id: 32, name: "Fareez Ahamed", points: 348 },
-    { id: 32, name: "Fareez Ahamed", points: 348 },
-    { id: 32, name: "Fareez Ahamed", points: 348 },
-    { id: 32, name: "Fareez Ahamed", points: 348 },
-    { id: 32, name: "Fareez Ahamed", points: 348 },
-    { id: 32, name: "Fareez Ahamed", points: 348 },
-  ]);
+export const getLeaderboard: () => Promise<
+  { id: number; name: string; points: number }[]
+> = () => {
+  return new Promise((resolve, reject) =>
+    setTimeout(
+      () =>
+        resolve([
+          { id: 32, name: "Fareez Ahamed", points: 348 },
+          { id: 32, name: "Fareez Ahamed", points: 348 },
+          { id: 32, name: "Fareez Ahamed", points: 348 },
+          { id: 32, name: "Fareez Ahamed", points: 348 },
+          { id: 34, name: "Fareez Ahamed", points: 348 },
+          { id: 34, name: "Fareez Ahamed", points: 348 },
+          { id: 32, name: "Fareez Ahamed", points: 348 },
+          { id: 32, name: "Fareez Ahamed", points: 348 },
+          { id: 32, name: "Fareez Ahamed", points: 348 },
+          { id: 32, name: "Fareez Ahamed", points: 348 },
+          { id: 32, name: "Fareez Ahamed", points: 348 },
+          { id: 32, name: "Fareez Ahamed", points: 348 },
+        ]),
+      1000
+    )
+  );
 };
 
 export const addMember = (data: { name: string }) => {
@@ -22,5 +30,11 @@ export const addMember = (data: { name: string }) => {
 export const login = (data: { username: string; password: string }) => {
   return new Promise((resolve, reject) =>
     setTimeout(() => reject("Invalid Credentials"), 1000)
+  );
+};
+
+export const addPoints = (data: { traineeId: number; points: number }) => {
+  return new Promise((resolve, reject) =>
+    setTimeout(() => resolve(null), 1000)
   );
 };
--- ui/src/pages/Leaderboard.tsx
+++ ui/src/pages/Leaderboard.tsx
@@ -1,18 +1,17 @@
 import styled from "@emotion/styled";
 import {
+  Alert,
   Button,
-  Grid,
+  LoadingOverlay,
   Modal,
-  Popover,
   SimpleGrid,
   Stack,
   Table,
-  Text,
 } from "@mantine/core";
-import { IconMenu2 } from "@tabler/icons";
-import { useQuery } from "@tanstack/react-query";
-import { useState } from "react";
-import { getLeaderboard } from "../api";
+import { IconCircleX, IconMenu2 } from "@tabler/icons";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMemo, useState } from "react";
+import { addPoints, getLeaderboard } from "../api";
 import { Layout } from "../components/Layout";
 import { Section } from "../components/Section";
 
@@ -34,68 +33,112 @@ const ActionCell = styled.td`
   max-width: 1rem;
 `;
 
-export const Leaderboard = () => {
-  const { isLoading, error, data } = useQuery(
-    ["leaderboardData"],
-    getLeaderboard
-  );
+const POINTS = [1, 2, 3, 5, 10, -1, -2, -3, -5, -10];
 
+export const Leaderboard = () => {
+  const [selectedTraineeId, setSelectedTraineeId] = useState<number>();
+  const [rewardedPoints, setRewardedPoints] = useState<number>();
   const [opened, setOpened] = useState(false);
 
-  const openPointsDialog = () => {
+  const query = useQuery(["leaderboardData"], getLeaderboard);
+
+  const addPointsMutation = useMutation(addPoints);
+
+  const traineeName = useMemo<string>(
+    () =>
+      query.data?.find((x) => x.id === selectedTraineeId)?.name ?? "No Name",
+    [query.data, selectedTraineeId]
+  );
+
+  const openPointsDialog = (traineeId: number) => {
+    setSelectedTraineeId(traineeId);
     setOpened(true);
   };
+
+  const handleCloseDialog = () => {
+    setSelectedTraineeId(undefined);
+    setOpened(false);
+  };
+
+  const handleAddPoints = (traineeId: number, points: number) => {
+    setRewardedPoints(points);
+    addPointsMutation.mutateAsync({ traineeId, points }).then(() => {
+      handleCloseDialog();
+      setRewardedPoints(undefined);
+      query.refetch();
+    });
+  };
+
+  const isRewardingPoints = (points: number) =>
+    rewardedPoints === points && addPointsMutation.isLoading;
 
   return (
     <>
       <Layout>
         <Section title="Leaderboard">
-          <Table fontSize={"lg"} verticalSpacing={"md"} highlightOnHover>
-            <thead>
-              <tr>
-                <NumericHeader>Rank</NumericHeader>
-                <NumericHeader>ID</NumericHeader>
-                <th>Name</th>
-                <NumericHeader>Points</NumericHeader>
-                <ActionHeader>Actions</ActionHeader>
-              </tr>
-            </thead>
-            <tbody>
-              {data?.map((row, i) => (
-                <tr key={row.name}>
-                  <NumericCell>{i + 1}</NumericCell>
-                  <NumericCell>{row.id}</NumericCell>
-                  <td>{row.name}</td>
-                  <NumericCell>{row.points}</NumericCell>
-                  <ActionCell>
-                    <Button size="xs" onClick={() => openPointsDialog()}>
-                      <IconMenu2 />
-                    </Button>
-                  </ActionCell>
+          {query.isFetched && (
+            <Table fontSize={"lg"} verticalSpacing={"md"} highlightOnHover>
+              <thead>
+                <tr>
+                  <NumericHeader>Rank</NumericHeader>
+                  <NumericHeader>ID</NumericHeader>
+                  <th>Name</th>
+                  <NumericHeader>Points</NumericHeader>
+                  <ActionHeader>Actions</ActionHeader>
                 </tr>
-              ))}
-            </tbody>
-          </Table>
+              </thead>
+              <tbody>
+                {query.data?.map((row, i) => (
+                  <tr key={row.name}>
+                    <NumericCell>{i + 1}</NumericCell>
+                    <NumericCell>{row.id}</NumericCell>
+                    <td>{row.name}</td>
+                    <NumericCell>{row.points}</NumericCell>
+                    <ActionCell>
+                      <Button
+                        size="xs"
+                        onClick={() => openPointsDialog(row.id)}
+                      >
+                        <IconMenu2 />
+                      </Button>
+                    </ActionCell>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          )}
+          {query.isLoading && <LoadingOverlay visible={query.isLoading} />}
+          {query.isError && (
+            <Alert icon={<IconCircleX size={18} />} title="Error" color="red">
+              {addPointsMutation.error as string}
+            </Alert>
+          )}
         </Section>
       </Layout>
       <Modal
         opened={opened}
-        onClose={() => setOpened(false)}
-        title="Reward Points"
+        onClose={handleCloseDialog}
+        title={`Reward Points to ${traineeName}`}
       >
         <Stack>
           <SimpleGrid cols={5}>
-            <Button>1</Button>
-            <Button>2</Button>
-            <Button>3</Button>
-            <Button>5</Button>
-            <Button>10</Button>
-            <Button color="red">-1</Button>
-            <Button color="red">-2</Button>
-            <Button color="red">-3</Button>
-            <Button color="red">-5</Button>
-            <Button color="red">-10</Button>
+            {POINTS.map((point) => (
+              <Button
+                color={point > 0 ? "blue" : "red"}
+                loading={isRewardingPoints(point)}
+                onClick={() =>
+                  selectedTraineeId && handleAddPoints(selectedTraineeId, point)
+                }
+              >
+                {point}
+              </Button>
+            ))}
           </SimpleGrid>
+          {addPointsMutation.isError && (
+            <Alert icon={<IconCircleX size={18} />} title="Error" color="red">
+              {addPointsMutation.error as string}
+            </Alert>
+          )}
         </Stack>
       </Modal>
     </>
